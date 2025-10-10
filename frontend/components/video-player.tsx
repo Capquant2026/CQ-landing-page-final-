a07a28bd9f17c688a99b5863a7531727e27e3c17
mohamed-elhaissan <mhalwasoffice@gmail.com>
2025-10-10 18:44:58 +0100

    updating

--- frontend/components/video-player.tsx
+++ frontend/components/video-player.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { Copy, Share2, X } from "lucide-react";
+import { useParams, usePathname } from "next/navigation";
 import { useState } from "react";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
+import { toast } from "sonner";
 
 const flags = [
   {
@@ -48,15 +50,17 @@ const flags = [
 ];
 
 export default function VideoPlayer() {
+  const currentUrl = typeof window !== "undefined" ? window.location.href : "";
+
   const [selected, setSelected] = useState(flags[0]);
   const [open, setOpen] = useState(false);
   const [showShareMenu, setShowShareMenu] = useState(false);
   const videoUrl = "https://delicate-bombolone-7a37e8.netlify.app/ma-video.mp4";
-  const shareUrl = "https://delicate-bombolone-7a37e8.netlify.app/ma-video.mp4";
 
   const handleCopy = async () => {
-    await navigator.clipboard.writeText(videoUrl);
-    alert("Link copied to clipboard!");
+    await navigator.clipboard.writeText(currentUrl);
+
+    toast.success("Link copied to clipboard ! ");
   };
 
   const socials = [
@@ -99,13 +103,13 @@ export default function VideoPlayer() {
   ];
 
   return (
-    <div className="w-full min-h-screen flex items-center justify-center p-4 sm:p-6 md:p-8">
+    <div className="w-full min-h-screen flex items-center justify-center p-4 sm:p-6 md:p-8 ">
       <div className="w-full max-w-4xl relative">
         <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center gap-3 mb-3 sm:mb-4">
-          <div className="relative w-full sm:w-auto">
+          <div className="relative  sm:w-auto">
             <button
               onClick={() => setOpen(!open)}
-              className="flex items-center justify-between  sm:w-auto bg-[#1b1b1d] border border-white/10 rounded-lg px-3 py-2 hover:bg-[#2a2a2c] transition"
+              className="flex items-center justify-between w-full sm:w-auto bg-[#1b1b1d] border border-white/10 rounded-lg px-3 py-2 hover:bg-[#2a2a2c] transition"
             >
               <div className="flex items-center gap-2 text-sm text-white">
                 <img
@@ -155,27 +159,30 @@ export default function VideoPlayer() {
               </ul>
             )}
           </div>
-
-          {/* Share Button */}
         </div>
 
-        <div className="relative w-full h-[300px] sm:h-[400px] md:h-[450px] lg:h-[500px] rounded-lg bg-gradient-to-b from-[#141516] to-[#08090A] flex items-center justify-center border border-white/10 overflow-hidden">
-          <iframe
-            src={videoUrl}
-            className="w-full h-full rounded-lg"
-            allowFullScreen
-            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          ></iframe>
+        <div
+          className="relative w-full rounded-lg bg-gradient-to-b from-[#141516] to-[#08090A] border border-white/10 "
+          style={{ paddingBottom: "56.25%" }}
+        >
+          <div className="absolute inset-0">
+            <iframe
+              src={videoUrl}
+              className="w-full h-full rounded-lg"
+              allowFullScreen
+              allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            ></iframe>
+          </div>
 
           <button
-            onClick={() => setShowShareMenu(!open)}
-            className="absolute top-4 right-4 bg-black/60 text-white p-2 rounded-full hover:bg-black/80 transition"
+            onClick={() => setShowShareMenu(!showShareMenu)}
+            className="absolute top-4 right-4 bg-black/60 text-white p-2 rounded-full hover:bg-black/80 transition z-20"
           >
             <Share2 className="size-5" />
           </button>
 
           {showShareMenu && (
-            <div className="absolute top-14 right-4 bg-[#1A1A1C] border border-white/10 rounded-xl p-3 flex flex-col gap-2 shadow-lg z-10 w-48">
+            <div className="absolute top-14 overflow-y-auto md:h-[280px]  right-4 bg-[#1A1A1C] border border-white/10 rounded-xl p-3 flex flex-col gap-2 shadow-lg z-50 w-48">
               <div className="flex justify-between items-center mb-2">
                 <span className="text-sm font-semibold text-white">
                   Share Video
@@ -194,7 +201,7 @@ export default function VideoPlayer() {
                   href={s.url}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className={`flex items-center gap-2 text-white px-3 py-2 rounded-lg ${s.color} hover:opacity-90 transition text-sm`}
+                  className={`flex items-center gap-2 text-white px-3 py-1 md:py-2 rounded-lg ${s.color} hover:opacity-90 transition text-xs md:text-sm`}
                 >
                   {s.icon}
                   {s.name}
